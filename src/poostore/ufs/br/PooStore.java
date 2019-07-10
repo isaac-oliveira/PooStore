3fb24821cd6300f35be23e85c0f4635806d9093c
isaac-oliveira <isaacsantos9876@gmail.com>
2019-07-10 15:59:47 -0300

    Exibindo opcoes

--- src/poostore/ufs/br/PooStore.java
+++ src/poostore/ufs/br/PooStore.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poostore.ufs.br;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +12,21 @@ public class PooStore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Escolha uma opção:\n" +
+                "   1. Listar todos os produtos cadastrados\n" +
+                "   2. Listar os produtos disponíveis no estoque\n" +
+                "   3. Listar todos os produtos esgotados no estoque\n" +
+                "   4. Aumentar a quantidade de produtos no estoque\n" +
+                "   5. Cadastrar um cliente\n" +
+                "   6. Listar os clientes cadastrados\n" +
+                "   7. Cadastrar vendas\n" +
+                "   8. Listar todas as vendas\n" +
+                "   9. Listar compras de um determinado cliente\n" + 
+                "   10. Sair do programa");
+        
+        System.out.print("Digite a opção desejada: ");
+        int opcao = input.nextInt();
     }
-    
 }
